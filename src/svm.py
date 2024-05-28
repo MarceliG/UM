a328a46f66a8ae80b318a339cb7005ffab44a7c1
MarceliG <marcel9817@gmail.com>
2024-05-28 23:21:57 +0200

    initial  Bayesian Optimization

--- src/svm.py
+++ src/svm.py
@@ -1,11 +1,153 @@
+from typing import Dict, Optional, Union
+
 import nltk
+import numpy as np
+import optuna
 import pandas as pd
 from nltk.corpus import stopwords
+from sklearn import svm
+from sklearn.datasets import load_iris
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics import accuracy_score, classification_report
-from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score, classification_report, make_scorer
+from sklearn.model_selection import StratifiedKFold, cross_val_score
 
 from text import split_data
+
+
+class SVMclassifier:
+    """Class to find the best parameters of the SVM model."""
+
+    def __init__(self) -> None:
+        pass
+
+    def create_model(
+        self,
+        c: float = 1.0,
+        kernel: str = "rbf",
+        degree: int = 3,
+        gamma: str = "scale",
+        coef0: float = 0.0,
+        shrinking: bool = True,
+        probability: bool = False,
+        tol: float = 0.001,
+        cache_size: int = 200,
+        class_weight: Union[Dict[int, float], str, None] = None,
+        verbose: bool = False,
+        max_iter: int = -1,
+        decision_function_shape: str = "ovr",
+        break_ties: bool = False,
+        random_state: Optional[int] = None,
+    ):
+        """
+        Creates an SVM model with specified hyperparameters.
+
+        Args:
+            c: Regularization parameter. The strength of the regularization is inversely proportional to C.
+            kernel: Specifies the kernel type to be used in the algorithm.
+                It must be one of 'linear', 'poly', 'rbf', 'sigmoid', 'precomputed', or a callable.
+            degree: Degree of the polynomial kernel function ('poly'). Ignored by all other kernels.
+            gamma: Kernel coefficient for 'rbf', 'poly', and 'sigmoid'.
+                If 'scale', it uses 1 / (n_features * X.var()), if 'auto', it uses 1 / n_features.
+            coef0: Independent term in kernel function. It is only significant in 'poly' and 'sigmoid'.
+            shrinking: Whether to use the shrinking heuristic.
+            probability: Whether to enable probability estimates. This must be enabled prior to calling fit,
+                and will slow down that method.
+            tol: Tolerance for stopping criterion.
+            cache_size: Size of the kernel cache (in MB).
+            class_weight: Set the parameter C of class i to class_weight[i]*C for SVC. If not given,
+                all classes are supposed to have weight one. The 'balanced' mode uses the values of y
+                to automatically adjust weights inversely proportional to class frequencies.
+            verbose: Enable verbose output. Note that this setting takes advantage of a per-process runtime setting
+                in libsvm that, if enabled, may not work properly in a multithreaded context.
+            max_iter: Hard limit on iterations within solver, or -1 for no limit.
+            decision_function_shape: Whether to return a one-vs-rest ('ovr') decision function of shape
+                (n_samples, n_classes) as all other classifiers, or the original one-vs-one ('ovo')
+                decision function of libsvm which has shape (n_samples, n_classes * (n_classes - 1) / 2).
+            break_ties: If true, `decision_function_shape`='ovr', and number of classes > 2,
+                `predict` will break ties according to the confidence values.
+            random_state: Controls the pseudo random number generation for shuffling the data for probability estimates.
+
+        Returns:
+            An SVM model configured with the specified parameters.
+        """
+        return svm.SVC(
+            C=c,
+            kernel=kernel,
+            degree=degree,
+            gamma=gamma,
+            coef0=coef0,
+            shrinking=shrinking,
+            probability=probability,
+            tol=tol,
+            cache_size=cache_size,
+            class_weight=class_weight,
+            verbose=verbose,
+            max_iter=max_iter,
+            decision_function_shape=decision_function_shape,
+            break_ties=break_ties,
+            random_state=random_state,
+        )
+
+    def create_models_with_all_kernels(self) -> Dict[str, svm.SVC]:
+        """
+        Creates SVM models with each kernel type.
+
+        Returns:
+            A dictionary of SVM models with keys as kernel names and values as the corresponding SVM model.
+        """
+        kernels = ["linear", "poly", "rbf", "sigmoid"]
+        models = {}
+
+        for kernel in kernels:
+            models[kernel] = self.create_model(kernel=kernel)
+
+        return models
+
+    def optimize_parameters(self, texts: np.ndarray, labels: np.ndarray, n_trials: int = 100) -> svm.SVC:
+        """
+        Use Optuna to optimize hyperparameters.
+
+        Args:
+            texts: Feature data.
+            labels: Target labels.
+            n_trials: Number of trials for the optimization.
+
+        Returns:
+            The best model with optimized parameters.
+        """
+        study = optuna.create_study(direction="maximize")
+        study.optimize(lambda trial: self.objective(trial, texts, labels), n_trials=n_trials)
+
+        best_params = study.best_params
+        return self.create_model(
+            c=best_params["C"],
+            kernel=best_params["kernel"],
+            degree=best_params.get("degree", 3),
+            gamma=best_params.get("gamma", "scale"),
+            coef0=best_params.get("coef0", 0.0),
+        )
+
+    def objective(self, trial: optuna.Trial, texts: np.ndarray, labels: np.ndarray) -> float:
+        """
+        Objective function for optimization.
+
+        Args:
+            trial: Optuna's Trial object.
+            texts: Feature data.
+            labels: Target labels.
+
+        Returns:
+            The accuracy score of the model with current hyperparameters.
+        """
+        c = trial.suggest_loguniform("C", 1e-10, 1e10)
+        kernel = trial.suggest_categorical("kernel", ["linear", "poly", "rbf", "sigmoid"])
+        degree = trial.suggest_int("degree", 1, 10)
+        gamma = trial.suggest_categorical("gamma", ["scale", "auto"])
+        coef0 = trial.suggest_uniform("coef0", -10, 10)
+
+        model = self.create_model(c=c, kernel=kernel, degree=degree, gamma=gamma, coef0=coef0)
+        scores = cross_val_score(model, texts, labels, cv=5)
+        return scores.mean()
 
 
 def main():
@@ -54,15 +196,24 @@ def main():
         labels_test,
     ) = split_data(texts=x_tfidf, labels=labels)
 
-    model = SVC(kernel="linear")
-    model.fit(texts_train, labels_train)
+    model = svm.SVC(kernel="linear")
 
-    labels_pred = model.predict(texts_test)
+    svm_classifier = SVMclassifier()
+    models = svm_classifier.create_models_with_all_kernels()
+    for model_name, model in models.items():
+        model.fit(texts_train, labels_train)
+        labels_pred = model.predict(texts_test)
 
-    print("Accuracy:", accuracy_score(labels_test, labels_pred))
-    print("Classification Report:")
-    print(classification_report(labels_test, labels_pred))
+        print(f"*****{model_name}*****")
+        print("Accuracy:", accuracy_score(labels_test, labels_pred))
+        print("Classification Report:")
+        print(classification_report(labels_test, labels_pred))
+        print()
 
 
 if __name__ == "__main__":
     main()
+
+
+# Upewnienie się, że dane są zbalansowane
+# Bayesian Optimization
